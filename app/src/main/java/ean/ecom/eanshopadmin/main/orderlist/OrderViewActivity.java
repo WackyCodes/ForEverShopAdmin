869017c0435e1e9e62aa78a5c219e6b945f80911
WackyCodes <wackycodes@gmail.com>
2020-09-14 18:24:01 +0530

    Commit No. 14 09 20 : 02

---
+++ app/src/main/java/ean/ecom/eanshopadmin/main/orderlist/OrderViewActivity.java
@@ -0,0 +1,28 @@
+package ean.ecom.eanshopadmin.main.orderlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ean.ecom.eanshopadmin.R;
+
+/*
+ * Copyright (c) 2020.
+ * WackyCodes : Tech Services.
+ * https://linktr.ee/wackycodes
+ */
+
+public class OrderViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_order_view );
+
+    }
+
+
+
+
+
+}
